5d61085679369987d434817d5deb63c176b1ab88
MitchelMA <32548@ma-web.nl>
2022-05-11 14:25:38 +0200

    config in de server-directory en navigator toegevoegd

---
+++ server/navigator.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import json
+
+CHROMEDRIVERPATH: str
+
+driver: webdriver
+
+# function to load in the local config file
+def loadLocaleConfig():
+    with open('./config.json') as f:
+        global CHROMEDRIVERPATH
+        jsn = json.load(f)
+        CHROMEDRIVERPATH = jsn['chromeDriverPath']
+
+
+# function to start the webdriver
+def startDriver():
+    # load in the config file to get the path to the chromedriver
+    loadLocaleConfig()
+    global driver
+    driver = webdriver.Chrome(CHROMEDRIVERPATH)
+
+
+# function to search through a list of url's
+def searchList(urls: list):
+    for i in range(0, len(urls)):
+        print(f"{i}: {urls[i]}");
+        driver.get(urls[i])
+        time.sleep(2)
+
+if __name__ == '__main__':
+    startDriver()
+    searchList(['http://32548.hosts1.ma-cloud.nl/Les09/'])
